03f308099e1918334e174ad70bfdb965a8a40dcc
Chelsie Edun <coedun1@sheffield.ac.uk>
2020-08-05 15:33:12 +0000

    Modification to regulation table now has references to the event table.

--- schema.sql
+++ schema.sql
@@ -7,10 +7,11 @@ CREATE TABLE country (
 CREATE TABLE regulation (
   id INTEGER,
   information TEXT NOT NULL,
-  event TEXT NOT NULL,
+  event id NOT NULL,
   country INTEGER NOT NULL,
   reference TEXT NOT NULL,
   FOREIGN KEY(country) REFERENCES country(id),
+  FOREIGN KEY(event) REFERENCES event(id),
   PRIMARY KEY (id)
 );
 
